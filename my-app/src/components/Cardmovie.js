74959a91e83ddb7991cb40cc70b5855991c33489
Clément Héré <clement.here.tech@gmail.com>
2019-11-02 19:15:47 +0100

    new changes

--- my-app/src/components/Cardmovie.js
+++ my-app/src/components/Cardmovie.js
@@ -7,11 +7,12 @@ const CardMovie = ({ movie }) => {
     <div className="container">
       <img 
         className="imgCharacter"
-        src={movie.posterUrl}/>
-        <p className="titleMovie">{movie.title}</p>
-        <p>By {movie.director} </p> 
-        <p>{movie.year}  </p>
-        <p>{movie.country}</p>
+        src={movie.posterUrl}
+      />
+      <p className="titleMovie">{movie.title}</p>
+      <p>By {movie.director} </p> 
+      <p>{movie.year}  </p>
+      <p>{movie.country}</p>
     </div>
   )
 }
